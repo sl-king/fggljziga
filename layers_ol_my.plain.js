6974f78001140f6809c670f151366efeb7e47793
333ZR <45237720+333ZR@users.noreply.github.com>
2025-07-11 15:00:32 +0200

    Update layers_ol_my.plain.js

--- layers_ol_my.plain.js
+++ layers_ol_my.plain.js
@@ -163,7 +163,7 @@
         
         if (!ly_sxid_geo_nacrt_style_cache.has(cacheKey)) {
           ly_sxid_geo_nacrt_style_cache.set(cacheKey, new olStyle({
-            image: new olstyle.Icon({
+            image: new olStyle.Icon({
               src: svgPath,
               scale: radius / 25,
               anchor: [0.5, 0.5],
